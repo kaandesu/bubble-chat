0d26a68f036b5a5785a4bdae1ee3443faea0faf0
kaan <kaandesu00@gmail.com>
2025-05-03 16:00:19 +0200

    feat(client): connect to the server send and recieve messages

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/textarea"
@@ -11,14 +12,26 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+const serverAddr = "127.0.0.1:3000"
+
 type (
-	errMsg error
-	model  struct {
-		viewport    viewport.Model
-		textarea    textarea.Model
-		messages    []string
-		senderStyle lipgloss.Style
-		err         error
+	errMsg      error
+	msgReceived struct {
+		from  string
+		value string
+	}
+	registerCon struct {
+		con net.Conn
+	}
+	model struct {
+		viewport      viewport.Model
+		textarea      textarea.Model
+		messages      []string
+		senderStyle   lipgloss.Style
+		recieverStyle lipgloss.Style
+		roomStyle     lipgloss.Style
+		con           net.Conn
+		err           error
 	}
 )
 
@@ -46,11 +59,13 @@ Type a message and press Enter to send.`)
 	ta.KeyMap.InsertNewline.SetEnabled(false)
 
 	return model{
-		textarea:    ta,
-		viewport:    vp,
-		messages:    []string{},
-		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
-		err:         nil,
+		textarea:      ta,
+		viewport:      vp,
+		messages:      []string{},
+		senderStyle:   lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		recieverStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("4")),
+		roomStyle:     lipgloss.NewStyle().Foreground(lipgloss.Color("3")),
+		err:           nil,
 	}
 }
 
@@ -68,6 +83,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.viewport, vpCmd = m.viewport.Update(msg)
 
 	switch msg := msg.(type) {
+	case msgReceived:
+		switch msg.from {
+		case "Room":
+			m.messages = append(m.messages, m.roomStyle.Render(msg.from+": ")+msg.value)
+		default:
+			m.messages = append(m.messages, m.recieverStyle.Render(msg.from+": ")+msg.value)
+		}
+		m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+	case registerCon:
+		m.con = msg.con
 	case tea.WindowSizeMsg:
 		m.viewport.Width = msg.Width
 		m.textarea.SetWidth(msg.Width)
@@ -87,6 +112,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, nil
 			}
 
+			if m.con != nil {
+				m.con.Write([]byte(m.textarea.Value() + "\n"))
+			}
 			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
 			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
 			m.textarea.Reset()
@@ -105,8 +133,42 @@ func (m model) View() string {
 	return fmt.Sprintf("%s%s%s", m.viewport.View(), gap, m.textarea.View())
 }
 
+func Connect(addr string, p *tea.Program) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	temp := make(chan struct{})
+
+	con, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err == nil {
+		p.Send(registerCon{
+			con: con,
+		})
+	}
+	go func() {
+		buf := make([]byte, 1024)
+		for {
+			n, err := con.Read(buf)
+			if err != nil {
+				break
+			}
+			if n < 1 {
+				continue
+			}
+			splits := strings.Split(string(buf[:n-1]), ">>")
+			from, payload := splits[0], splits[1]
+			p.Send(msgReceived{from: from, value: payload})
+		}
+	}()
+	<-temp
+}
+
 func main() {
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+
+	go Connect(serverAddr, p)
 
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
